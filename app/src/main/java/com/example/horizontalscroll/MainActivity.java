2513283a9e9b2f89deed2b1005cc0266147eba5f
Soham Samanta <sohamsamanta2@gmail.com>
2021-08-07 20:09:05 +0530

    initial build

---
+++ app/src/main/java/com/example/horizontalscroll/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.horizontalscroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome to Soham's App", Toast.LENGTH_SHORT).show();
+                Log.d("General","The button is working properly!");
+            }
+        });
+
+    }
+}
